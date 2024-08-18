fa6d54dbeb0c5f026b5b2a4efd33ad5dd2274dc3
jnlcc683 <jnlcc683@gmail.com>
2024-08-18 14:07:57 +0900

    0818 207 recreate

---
+++ js/slideable.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $(".left1>li").click(function(){
+    $(".left1>li").removeClass("on")
+    $(this).addClass("on")
+    let idx = $(this).index()
+    console.log(sideListCar[idx].carList)
+    let result = ""
+    for(let i=0 ; i<sideListCar[idx].carList.length ; i++){
+      result+=`<li>
+        <img src="./img/${sideListCar[idx].carList[i].imgSrc}" alt="${sideListCar[idx].carList[i].carName}">
+        <p>${sideListCar[idx].carList[i].carName}</p>
+      </li>`
+    }
+    console.log(result)
+    $(".right1").html(result)
+
+  })
+
+
+  $(".btn_gnb_menu").click(function(){
+    $(".sideGnb").addClass("on")
+  })
+  $(".top_close_btn").click(function(){
+    $(".sideGnb").removeClass("on")
+  })
+
+
+
+})
